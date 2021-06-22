5b32926bed4d7167d8b25819365733646b145f78
IgooorGP <igor.feq@gmail.com>
2021-06-21 23:54:29 -0300

    feat: centralize clear screen color

--- src/settings.py
+++ src/settings.py
@@ -9,6 +9,7 @@ ROOT_DIR: Path = Path(__file__).resolve().parent.parent  # root of the project (
 WINDOW_TITLE = "Pygame Lab!"
 WINDOW_SIZE: Tuple[int, int] = (600, 400)
 RAW_DISPLAY_SIZE: Tuple[int, int] = (300, 200)
+CLEAR_DISPLAY_RGB: Tuple[int, int, int] = (128, 163, 224)
 GAME_FPS = 60
 TILE_SIZE = 16
 
